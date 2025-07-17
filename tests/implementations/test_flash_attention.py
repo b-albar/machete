3c21103451fd074013d66aa9712b14d625b6f470
Boris Albar <boris.albar@protonmail.com>
2025-07-17 14:19:41 +0200

    fix: update test for l_vec

--- tests/implementations/test_flash_attention.py
+++ tests/implementations/test_flash_attention.py
@@ -21,16 +21,20 @@ def test_attention_fwd(b, h, m, n, d, causal, dtype, scale):
     k = torch.empty((b, h, n, d), dtype=dtype, device="cuda").normal_(mean=0., std=0.1)
     v = torch.empty((b, h, n, d), dtype=dtype, device="cuda").normal_(mean=0., std=0.1)
 
-    o_ref, _ = attn_ref(q, k, v, causal=causal, sm_scale=scale, upcast=True)
-    o_torch, _ = attn_ref(q, k, v, causal=causal, sm_scale=scale, upcast=False)
+    o_ref, l_vec_ref = attn_ref(q, k, v, causal=causal, sm_scale=scale, upcast=True)
+    o_torch, l_vec_torch = attn_ref(q, k, v, causal=causal, sm_scale=scale, upcast=False)
     #o_spda = torch.nn.functional.scaled_dot_product_attention(q, k, v, is_causal=causal, scale=scale)
     #o_fa2 = flash_attn_func(q, k, v, softmax_scale=scale, causal=causal)
-    o_hyp, _ = flash_attention(q, k, v, causal, scale)
+    o_hyp, l_vec_hyp = flash_attention(q, k, v, causal, scale)
 
     machete_max_diff = max_diff(o_ref, o_hyp)
     ref_max_diff = max_diff(o_ref, o_torch)
 
+    l_vec_max_diff = max_diff(l_vec_ref, l_vec_hyp)
+    l_vec_ref_max_diff = max_diff(l_vec_ref, l_vec_torch)
+
     assert machete_max_diff < 2 * ref_max_diff
+    assert l_vec_max_diff < 2 * l_vec_ref_max_diff
 
 @pytest.mark.parametrize('scale', [0.125])
 @pytest.mark.parametrize('b', [2, 3, 5])
@@ -100,4 +104,5 @@ def test_attention_bwd(b, h, m, n, d, causal, dtype, scale):
     assert dk_diff < 2 * dk_diff_ref, f"dK difference too large: {dk_diff} vs {dk_diff_ref}"
     assert dv_diff < 2 * dv_diff_ref, f"dV difference too large: {dv_diff} vs {dv_diff_ref}"
 
+test_attention_fwd(3, 16, 128, 128, 64, False, torch.bfloat16, 0.125)
 #test_attention_bwd(3, 16, 128, 128, 64, False, torch.bfloat16, 0.125)
